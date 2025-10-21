d3c129f78fb7f24cb69ada5fce538c74b53d1bc4
keyobs <elodiekoob@gmail.com>
2025-10-21 11:55:23 +0200

    chore(config): refacto tsconfig and vite config according to the project

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,25 @@
+import path from "node:path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+const basePath = "/rdbc/";
 
 export default defineConfig({
-  base: "/rdbc/",
+  base: basePath,
   plugins: [
     react({
       babel: {
         plugins: [["babel-plugin-react-compiler"]],
       },
     }),
+    tsconfigPaths(),
   ],
+  css: {
+    preprocessorOptions: {
+      less: {
+        paths: [path.resolve(__dirname, "src")],
+      },
+    },
+  },
 });
